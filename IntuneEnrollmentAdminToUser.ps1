6a68c28e9e99dc99f625faee1096cf51dc2ce126
Letlaka Tsotetsi <Letlaka.t@gmail.com>
2023-09-14 09:48:08 +0200

    Add files via upload

---
+++ IntuneEnrollmentAdminToUser.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+Param(
+    [string]$UserName = (Get-WmiObject -Class Win32_ComputerSystem).UserName
+)
+Begin{
+    # Verify that the script is running with administrative privileges
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Host "This script must be run as an Administrator. Please re-run this script as an Administrator."
+        exit
+    }
+}
+Process
+{
+    try {
+        if (net localgroup administrators | Select-String $UserName -SimpleMatch){
+            $confirmation = Read-Host "Are you sure you want to remove $UserName from the Administrators group? (yes/no)"
+            if ($confirmation -eq 'yes') {
+                net localgroup administrators $($UserName) /delete 
+                Write-Host "$UserName has been removed from the Administrators group."
+            } else {
+                Write-Host "Operation cancelled."
+            }
+        }
+        else{
+            Write-Host "$UserName is not a member of the Administrators group."
+        }
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
+End{
+    Write-Host "Script execution completed."
+}
